21a7d904effc97439ce12bdf46e1faa4b8dddd41
jaydks <jjeee1313@gmail.com>
2022-08-24 04:58:44 +0900

    RecyclerView_ViewBinding_클릭 이벤트 추가

--- app/src/main/java/com/example/practicerecyclerview/ProfileAdapter.kt
+++ app/src/main/java/com/example/practicerecyclerview/ProfileAdapter.kt
@@ -2,6 +2,7 @@ package com.example.practicerecyclerview
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.practicerecyclerview.databinding.ListItemBinding
 
@@ -11,7 +12,14 @@ class ProfileAdapter(val profileList: ArrayList<Profiles>) : RecyclerView.Adapte
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileAdapter.CustomViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-        return CustomViewHolder(ListItemBinding.bind(view)) // binding을 access하기 위해 ListItemBinding.bind(view)
+        return CustomViewHolder(ListItemBinding.bind(view)).apply {
+            binding.root.setOnClickListener {
+                val curPos: Int = adapterPosition
+                val profile: Profiles = profileList.get(curPos)
+                Toast.makeText(parent.context, "이름 : ${profile.name}\n나이 : ${profile.age}\n직업 : ${profile.job}", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
