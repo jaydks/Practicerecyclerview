6c887f859229d8cd568207cfe0d4995cc88ad72f
jaydks <jjeee1313@gmail.com>
2022-08-24 07:26:04 +0900

    RecyclerView_ViewBinding_리팩토링_체크박스 이슈 해결

--- app/src/main/java/com/example/practicerecyclerview/ProfileAdapter.kt
+++ app/src/main/java/com/example/practicerecyclerview/ProfileAdapter.kt
@@ -11,6 +11,7 @@ import kotlin.collections.ArrayList
 
 class ProfileAdapter : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>() {
     private val profileList= arrayListOf<Profiles>()
+    private val userCheckBoxStatus = arrayListOf<UserCheckStatus>()
 
     fun removeData(position: Int){
         profileList.removeAt(position)
@@ -25,7 +26,8 @@ class ProfileAdapter : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>() {
 
     fun addData(gender: Int, name: String, age: Int, job: String) {
         profileList.add(Profiles(gender, name, age, job))
-        notifyItemInserted(profileList.size)
+        userCheckBoxStatus.add(UserCheckStatus((profileList.size - 1), false))
+        notifyItemInserted(profileList.size-1)
     }
 
     override fun onCreateViewHolder(
@@ -37,21 +39,27 @@ class ProfileAdapter : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        holder.bind(profileList[position])
+        holder.bind(profileList[position], userCheckBoxStatus[position])
     }
 
     override fun getItemCount() = profileList.size
 
     class CustomViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind(profile: Profiles) = with(binding){
+        fun bind(profile: Profiles, userStatus: UserCheckStatus) = with(binding){
             ivProfile.setImageResource(profile.gender)
             tvName.text = profile.name
             tvAge.text = profile.age.toString()
             tvJob.text = profile.job
             tvNum.text = "${layoutPosition}번째"
 
+            checkBox.isChecked = userStatus.isChecked
+
             itemlayout.setOnClickListener {
                 Toast.makeText(it.context,"이름 : ${profile.name}\n나이 : ${profile.age}\n직업 : ${profile.job}", Toast.LENGTH_SHORT).show()
+            }
+
+            checkBox.setOnClickListener {
+                userStatus.isChecked = checkBox.isChecked
             }
         }
     }
